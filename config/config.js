c595efc18e733de880a284ae6a3af41ee78bcdb5
Carmen <carmen.morales.bonet@gmail.com>
2022-04-19 15:16:01 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-export const API_KEY = `952bee6a019f4e20b543a6a240571664`;
+export const API_KEY = ``;
 export const endpoint = `https://newsapi.org/v2/top-headlines`;
 export const country = 'es'
 export const language = 'es'
